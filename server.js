c7e0f47c61e8367b0911014cd9f1c53fd15d56ef
Quajo Duke <quajoduke305@gmail.com>
2020-09-09 10:52:16 +0000

    new changes

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const checkJwt = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: `https://dev-tv04xmhk.us.auth0.com/.well-known/jwks.json`,
+    jwksUri: `https://mikebuildingprojects.auth0.com/.well-known/jwks.json`,
   }),
 
   // Validate the audience and the issuer.
